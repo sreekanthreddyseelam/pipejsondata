c2902b3cb79214be77dbc6664933e38f739e8c1d
unknown <ssrikanthreddy999@gmail.com>
2020-06-22 09:50:03 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'basic04';
+  PageTitle:String = "pipes in angular";
+  users:any[]=[
+    {id:101,name:'john',city:'Leipzig',salary:200, dob: new Date('05/10/1996')},
+    {id:102,name:'peter',city:'Berlin',salary:2000, dob: new Date('12/09/1985')},
+    {id:103,name:'moni',city:'Hamburg',salary:5000, dob: new Date('02/07/1989')},
+    {id:104,name:'smith',city:'Munich',salary:4500, dob: new Date('11/04/1991')},
+    {id:105,name:'kunal',city:'Stuttgart',salary:3500, dob: new Date('01/10/1988')}
+
+
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AgePipe } from './pipes/age.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgePipe
   ],
   imports: [
     BrowserModule
